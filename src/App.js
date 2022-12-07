3f80d922ac363b1ebcc47afb140039a8e20fba2a
Agustina <agus.carrion@icloud.com>
2022-12-07 20:55:30 +0100

    added mapmarkers and geoJson

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import HomePage from "./pages/HomePage";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
   return (
     <main className="flex flex-col w-full h-full">
-      <HomePage/>
+      <HomePage />
     </main>
   );
 }
--- src/components/body/Map.js
+++ src/components/body/Map.js
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import geoJson from "../../data/mapLocations.json";
+
 mapboxgl.accessToken =
   "pk.eyJ1IjoiYW5kaXZ3aGl0ZSIsImEiOiJjbGIzeDQ4MmgwNXFmM3JxbnNlaW9neXc0In0.kyEmO_woTD50qizMwbHYmQ";
 
@@ -10,14 +12,21 @@ function Map() {
   const [lat, setLat] = useState(53.5488);
   const [zoom, setZoom] = useState(9);
 
+  // initialize map only once
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+    if (map.current) return;
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v12",
       center: [lng, lat],
       zoom: zoom,
     });
+    //create default markers
+    geoJson.features.map((feature) =>
+      new mapboxgl.Marker()
+        .setLngLat(feature.geometry.coordinates)
+        .addTo(map.current)
+    );
   });
 
   //   useEffect(() => {
@@ -27,7 +36,11 @@ function Map() {
   //     setLat(map.current.getCenter().lat.toFixed(4));
   //     setZoom(map.current.getZoom().toFixed(2));
   //     });
-  //   });
+  // });
+
+  //Clean up on unmount
+  //     return () => map.remove();
+  //   }, []);
 
   return (
     <>
