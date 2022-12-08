bec0be92c30d357dbedf6b6573c3ed80ad48eb8b
Tina <109415127+Agustina-Carrion@users.noreply.github.com>
2022-12-08 12:58:17 +0100

    added routing (#6)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,35 @@
-import NavBar from "../components/navbar/NavBar.js";
-import Footer from "../components/footer/Footer.js";
-import Body from "../components/body/Body.js";
-
+import { NavBar, Footer, Map, BlogCard } from "../componentExports";
+import myData from "../data/destinationsLight.json";
 
 function HomePage() {
+  const destinations = myData;
+
   return (
     <>
       <NavBar />
-      <Body />
+      <div className="relative bg-gradient-to-r from-cyan-400 to-orange-500">
+        <div className="grid sm:auto-cols-max md:grid-cols-2 justify-center items-center max-w-full min-h-screen px-4 py-8 sm:px-6 lg:px-8 lg:py-0">
+          <div className="grid grid-cols-2 md:ml-0 md:w-full md:px-8 md:py-16 sm:px-6 sm:py-12">
+            {destinations?.features
+              ?.sort(
+                (itemA, itemB) =>
+                  Number(itemB.properties.date) - Number(itemA.properties.date)
+              )
+              .map((item) => (
+                <BlogCard
+                  key={item.properties.title}
+                  image={item.properties.image}
+                  title={item.properties.title}
+                  dates={item.properties.date}
+                  author={item.properties.authorName}
+                />
+              ))}
+          </div>
+          <div className="md:w-full">
+            <Map />
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
---
+++ src/pagesExports.js
@@ -0,0 +1,3 @@
+export { default as HomePage } from "./pages/HomePage";
+export { default as BlogDetail } from "./pages/BlogDetail";
+export { default as Contact } from "./pages/Contact";
