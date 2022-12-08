a8dc4f16a8c29b83f74fa7d0f71c9c8e2b944515
Agustina <agus.carrion@icloud.com>
2022-12-08 11:38:53 +0100

    created new json file, added sorting to blogcards

--- src/components/body/BlogCard.js
+++ src/components/body/BlogCard.js
@@ -1,14 +1,10 @@
-function BlogCard({image, title, dates, author}) {
+function BlogCard({ image, title, dates, author }) {
   return (
     <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 m-3 ">
-      <img
-        className="rounded-t-lg"
-        src={image}
-        alt=""
-      />
+      <img className="rounded-t-lg h-1/2 w-full" src={image} alt="" />
       <div className="p-5">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-         {title}
+          {title}
         </h5>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {dates}
