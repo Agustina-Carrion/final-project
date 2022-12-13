8f366b3e9a0fea2cd4f5b5ba656d2f40da758e9e
Andi White <andi.v.white@gmail.com>
2022-12-13 12:11:44 +0100

    added useEffect to link to RestAPI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
+import { useState, useEffect} from "react";
 import { HomePage, Contact, BlogDetail } from "./pagesExports";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { Switch, Route } from "wouter";
-import data from "./data/destinationsLightMode.json";
+//import data from "./data/destinationsLightMode.json";
 
 function App() {
-  const destinations = data;
+  //const destinations = data;
+  const [travelDestinations, setTravelDestinations] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect( () => {
+    fetch ("http://localhost:3001/destinations")
+    .then ((response) => response.json())
+    .then ((result) => {
+      setTravelDestinations (result);
+      console.log(result);
+      setError (null);
+    })
+    .catch (setError);
+  },[]);
 
   return (
     <main className="flex flex-col w-full h-full">
       <Switch>
         <Route path="/blog/:id">
-          {(params) => (
+          {/* {(params) => (
             <BlogDetail id={params.id} destinations={destinations} />
+          )} */}
+             {(params) => (
+            <BlogDetail id={params.id} destinations={travelDestinations} />
           )}
         </Route>
         <Route path="/contact" component={Contact} />
         <Route>
-          <HomePage destinations={destinations} />
+          {/* <HomePage destinations={destinations} /> */}
+          <HomePage error={error} destinations={travelDestinations} />
         </Route>
       </Switch>
     </main>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,27 +1,30 @@
 import { NavBar, Footer, MapContainer, BlogCard } from "../componentExports";
+import gif from "../assets/Lost -The Weeknd.gif"; 
 
-function HomePage({ destinations }) {
+
+function HomePage({ error, destinations }) {
   return (
     <>
       <NavBar />
       <div className="relative bg-rose-500 dark:bg-slate-900">
         <div className="grid sm:auto-cols-max md:grid-cols-2 justify-center items-center px-4 py-8 sm:px-6 lg:px-8 lg:py-0">
           <div className="grid grid-cols-2 md:ml-0 md:w-full md:px-0 md:py-16 sm:px-6 sm:py-12">
-            {destinations?.sort(
-                (itemA, itemB) =>
-                  Number(itemB.date) - Number(itemA.date)
-              )
-              .map((item, i) => (
-                <BlogCard
-                  key={item.title}
-                  id={i}
-                  image={item.image}
-                  title={item.title}
-                  date={item.date}
-                  authorImage={item.authorImage}
-                  author={item.author}
+            {error ? (<>
+            <h1 className="text-white text-xl text-center ">Error when retrieving data. We're trying to find out what happened...</h1>
+            <img className="px-5"src= {gif} alt="The Weekend.gif."/>
+            </>)
+              :
+                (destinations?.sort((itemA, itemB) => Number(itemB.date) - Number(itemA.date)).map((item, i) => (<BlogCard
+                key={item.title}
+                id={i}
+                image={item.image}
+                title={item.title}
+                date={item.date}
+                authorImage={item.authorImage}
+                author={item.author}
                 />
-              ))}
+                ))
+                )}
           </div>
           <div className="md:w-full">
             <MapContainer />
